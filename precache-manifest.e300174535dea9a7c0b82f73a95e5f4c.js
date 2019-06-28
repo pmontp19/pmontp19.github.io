be226c9b211a450735c2eaab14c2a29ec93df7b6
Pere <pere.montpeo@decathlon.com>
2019-06-28 09:22:00 +0200

    Updates

---
+++ precache-manifest.e300174535dea9a7c0b82f73a95e5f4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1645cf88b4fe213593ae7b6b597ec60c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e419fe578cd72d289ab",
+    "url": "/static/css/main.9f135823.chunk.css"
+  },
+  {
+    "revision": "400674f710ba018aaea8",
+    "url": "/static/js/2.7b6ada78.chunk.js"
+  },
+  {
+    "revision": "9e419fe578cd72d289ab",
+    "url": "/static/js/main.ed3392ad.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.e300174535dea9a7c0b82f73a95e5f4c.js"
 );
 
 self.addEventListener('message', (event) => {
